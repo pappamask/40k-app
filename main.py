f75239c65d0309887a5c4f88add12da97ab5342f
pappamask <troels.stoklund.larsen@gmail.com>
2025-02-01 22:39:45 +0100

    test

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
 import streamlit as st
+from PIL import Image
+
+im = Image.open('sally.ico')
+st.set_page_config(
+    page_title='Grimdank app for kill team scout squad',
+    layout='wide',
+    initial_sidebar_state='expanded',
+    page_icon=im,
+    menu_items=None
+)
 
 #### Faction rules ####
 with st.sidebar.expander('FACTION RULES'):
     st.title('FORWARD SCOUTING')
-    redeploy = st.checkbox('Redeploy (1)', help='Change the set up of one third of your operatives (rounding up).')
-    reposition = st.checkbox('Reposition (2)',)
-    trip_alarm = st.checkbox('Trip Alarm (2)')
-    booby_trap = st.checkbox('Booby Trap (1)')
-    diversion = st.checkbox('Diversion (1)')
-    devise_plan = st.checkbox('Devise Plan (1)')
-    designate_target = st.checkbox('Designate Target (1)')
-    spy = st.checkbox('Spy (1)')
-    tactical_manoeuvre = st.checkbox('Tactical Manoeuvre (1)')
+    redeploy = st.checkbox('Redeploy (1)', 
+        help='''Change the set up of one third of your operatives
+        (rounding up).''')
+    reposition = st.checkbox('Reposition (2)',
+        help='''Perform a free Reposition action with one friendly operative  
+that’s wholly within your drop zone. It must end that move  
+wholly within 3" of your drop zone.''')
+    trip_alarm = st.checkbox('Trip Alarm (2)',
+        help='''Place one of your Trip Alarm markers more than 6" from your  
+opponent’s drop zone. During the first and second turning  
+point, whenever a friendly SCOUT SQUAD operative is  
+shooting an enemy operative that’s within 2" of that marker,  
+that friendly operative’s ranged weapons have the Seek  
+weapon rule. In the Ready step of the third Strategy phase,  
+remove that marker.''')
+    booby_trap = st.checkbox('Booby Trap (1)',
+        help='''Place one of your Booby Trap markers more than 6" from your  
+opponent’s drop zone and more than 2" from other markers,  
+access points and Accessible terrain. The first time your  
+Booby Trap marker is within an enemy operative’s control  
+range, remove that marker and inflict 2D3 damage on that  
+operative; if it isn’t incapacitated, end its action (if any), even  
+if that action’s effects aren’t fulfilled. If it cannot be placed,  
+move it the minimum amount to do so.''')
+    diversion = st.checkbox('Diversion (1)',
+        help='''Once per battle STRATEGIC GAMBIT. Select one enemy   
+operative within 6" of a killzone edge. Until the end of that  
+operative’s next activation, subtract 1 from its APL stat.''')
+    devise_plan = st.checkbox('Devise Plan (1)',
+        help='''You gain 1CP.''')
+    designate_target = st.checkbox('Designate Target (1)',
+        help='''Select one enemy operative to gain one of your Target tokens.  
+Whenever a friendly SCOUT SQUAD operative is shooting  
+against, fighting against or retaliating against an enemy  
+operative that has one of your Target tokens, you can re-roll  
+one of your attack dice.''')
+    spy = st.checkbox('Spy (1)',
+        help='''Approved Ops only. Your opponent must reveal their selected  
+tac op.''')
+    tactical_manoeuvre = st.checkbox('Tactical Manoeuvre (1)',
+        help='''Once per battle STRATEGIC GAMBIT. Select one friendly  
+operative. Until the end of that operative’s next activation, add  
+1 to its APL stat.''')
 
 #### Faction equipment ####
 with st.sidebar.expander('FACTION EQUIPMENT'):
@@ -167,4 +211,4 @@ change its order.
 #### UNITS ####
 with st.expander('DATACARDS'):
     with st.container(border=True):
-        st.text_area('Sergeant bob')
+        st.write('Sergeant bob')
